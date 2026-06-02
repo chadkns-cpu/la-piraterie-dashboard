1d7f79f224726e366b11fc05a5d216f0d7e31a01
chadkns-cpu <sebastienpadou11@gmail.com>
2026-06-02 20:39:49 +0200

    Update balances.js

--- balances.js
+++ balances.js
@@ -63,7 +63,7 @@ const escapeHtml = (s) => String(s)
 
 // Change BALANCES_API_URL to your API endpoint.
 // IMPORTANT: the endpoint must allow browser requests (CORS).
-const BALANCES_API_URL = 'balances.json';
+const BALANCES_API_URL = 'https://control.katabump.com/server/9956fc2a';
 
 const load = async () => {
   try {
